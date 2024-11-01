aaf9a58c48d9a97e403f7496280f109aa93845d9
dehmovlaei <db.dehmovlaei@gmail.com>
2024-11-01 22:06:39 +0330

    ver1.1

---
+++ FastAPI/1.2. Introduction to API with Auth/README.md
@@ -0,0 +1,21 @@
+# PyDeploy By:
+### AmirHossein Dehmovlaei
+
+---
+## simple API Training
+in this python command line program we are using an Argument
+for get plant name from user. this name passing to API
+for generating an image depend on user inputted.
+then this picture sent to another API for detect plant name
+from its image and finally print name
+
+## Run
+1. clone the project
+2. install dependencies:
+```bash
+pip install -r requirements.txt
+```
+3. from project root directory runs:
+```bash
+ python main.py --plantName <your plantName>
+```
--- FastAPI/1.2. Introduction to API with Auth/main.py
+++ FastAPI/1.2. Introduction to API with Auth/main.py
@@ -5,11 +5,6 @@ import fal_api
 import plant_api
 
 def main():
-    # if len(sys.argv) > 1:
-    #     input_string = ' '.join(sys.argv[1:])
-    #     print("Input flower name:", input_string)
-    # else:
-    #     print("Please provide a string as input.")
     parser = argparse.ArgumentParser()
     parser.add_argument('--greeting', default='Hello My friend Your Fav Plants name is')
     parser.add_argument('--plantName', required=True, type=str)
@@ -25,15 +20,15 @@ def main():
     if response.status_code == 200:
         with open(plant_file_address, 'wb') as f:
             f.write(response.content)
-        print("Image downloaded as:", plant_name)
+        print("Image downloaded")
     else:
         print("Failed_Status code:", response.status_code)
 
-    img = cv2.imread(plant_name)
+    img = cv2.imread(plant_file_address)
     cv2.imshow("drawn plant", img)
     cv2.waitKey(0)
-    plant_file_2_name = plant_api.plant(plant_file_address)
-    print(f"your plant name: {plant_file_2_name}")
+    plant_img_2_name = plant_api.plant(plant_file_address)
+    print(f"your plant name: {plant_img_2_name}")
 
 if __name__ == "__main__":
     main()
